04a879869f95f9a5c6a0e81a52597ed4911fdc53
Mikhail Kondrashov <43527326+mihailkondrashov@users.noreply.github.com>
2019-05-12 20:13:15 +0800

    Create Classes IdentifiedObject and StaticLoadCharacteristic

---
+++ ApplicationInformationModel/IdentifiedObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ApplicationInformationModel
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class IdentifiedObject
+    {
+        /// <summary>
+        /// The aliasName is free text human readable name of the object alternative to IdentifiedObject.name
+        /// </summary>
+        public string AliasName { get; set; } = String.Empty;
+
+        /// <summary>
+        /// The description is a free human readable text describing or naming the object
+        /// </summary>
+        public string Description { get; set; } = String.Empty;
+
+        /// <summary>
+        /// The localName is a human readable name of the object
+        /// </summary>
+        public string LocalName { get; set; } = String.Empty;
+
+        /// <summary>
+        /// A Model Authority issues mRIDs
+        /// </summary>
+        public Guid MRID { get; }
+
+        /// <summary>
+        /// The name is a free text human readable name of the object
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        ///  The pathname is a system unique name composed from all IdentifiedObject.localNames in a naming hierarchy path from the object to the root
+        /// </summary>
+        public string PathName { get; set; } = String.Empty;
+
+        public IdentifiedObject(Guid mRid, string name)
+        {
+            this.MRID = mRid;
+            this.Name = name;
+        }
+    }
+}
---
+++ ApplicationInformationModel/StaticLoadCharacteristic.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ApplicationInformationModel
+{
+    public class StaticLoadCharacteristic : IdentifiedObject
+    {
+        /// <summary>
+        ///  Indicates the exponential voltage dependency model (pVoltateExponent and qVoltageExponent) is to be used
+        /// </summary>
+        public bool ExponentModel { get;}
+
+        /// <summary>
+        /// Portion of active power load modeled as constant current
+        /// </summary>
+        public double PConstantCurrent { get; set; }
+
+        /// <summary>
+        /// Portion of reactive power load modeled as constant current
+        /// </summary>
+        public double QConstantCurrent { get; set; }
+
+        /// <summary>
+        /// Portion of active power load modeled as constant power
+        /// </summary>
+        public double PConstantPower { get; set; }
+
+        /// <summary>
+        /// Portion of reactive power load modeled as constant power
+        /// </summary>
+        public double QConstantPower { get; set; }
+
+        /// <summary>
+        /// Portion of active power load modeled as constant impedance
+        /// </summary>
+        public double PConstantImpendance { get; set; }
+
+        /// <summary>
+        /// Portion of reactive power load modeled as constant impedance
+        /// </summary>
+        public double QConstantImpendance { get; set; }
+
+        /// <summary>
+        /// Exponent of per unit frequency effecting active power
+        /// </summary>
+        public double PFrequencyExponent { get; set; }
+
+        /// <summary>
+        /// Exponent of per unit voltage effecting real power
+        /// </summary>
+        public double PVoltageExponent { get; set; }
+
+        /// <summary>
+        /// Exponent of per unit frequency effecting reactive power
+        /// </summary>
+        public double QFrequencyExponent { get; set; }
+
+        /// <summary>
+        /// Exponent of per unit voltage effecting reactive power
+        /// </summary>
+        public double QVoltageExponent { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mRid"></param>
+        /// <param name="name"></param>
+        public StaticLoadCharacteristic(Guid mRid, string name) : base(mRid, name)
+        {
+        }
+    }
+}
