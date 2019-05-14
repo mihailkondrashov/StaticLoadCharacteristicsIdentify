81ffe4701f8319e8010828a6a9344c66bdd8c99f
Михаил Кондрашов <mihail-arhangel0@mail.ru>
2019-05-14 11:38:53 +0300

    Add additional tests

---
+++ ApplicationInformationModelTests/Controllers/EnergyConsumerControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApplicationInformationModel.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationInformationModel.Controllers.Tests
+{
+    [TestClass()]
+    public class EnergyConsumerControllerTests
+    {
+        [TestMethod()]
+        public void EnergyConsumerControllerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
