4275690452021f4d2e883421cbb92ba57926b69c
Михаил Кондрашов <mihail-arhangel0@mail.ru>
2019-05-23 06:42:52 +0300

    Add test

---
+++ ApplicationInformationModelTests/Controllers/DeskretizationValueControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApplicationInformationModel.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationInformationModel.Controllers.Tests
+{
+    [TestClass()]
+    public class DeskretizationValueControllerTests
+    {
+
+        [TestMethod()]
+        public void GetDeskretizationValuesTest()
+        {
+            Guid deskretizationValueMrid = Guid.NewGuid();
+            string measurementValue_name = "GetDeskretizationValuesTest";
+            double sensor = 0.2;
+            DateTime date = DateTime.Now;
+            double value = 10;
+
+            var deskretizationValue = new DeskretizationValueController();
+
+
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetMeasurementValueSourceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetAnalogTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
